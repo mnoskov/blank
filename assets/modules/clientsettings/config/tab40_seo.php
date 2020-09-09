e4b7bbe9170e781779ddf28d5678d7573bbd8bc2
mnoskov <nm@perm-tools.ru>
2020-09-09 16:36:10 +0500

    global meta properties

---
+++ assets/modules/clientsettings/config/tab40_seo.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'caption' => 'SEO',
+    'settings' => [
+        'meta_description' => [
+            'caption' => 'META description',
+            'type'  => 'textareamini',
+        ],
+        'meta_keywords' => [
+            'caption' => 'META keywords',
+            'type'  => 'textareamini',
+        ],
+        'og_title' => [
+            'caption' => 'OpenGraph title',
+            'type' => 'text',
+        ],
+        'og_image' => [
+            'caption' => 'OpenGraph image',
+            'type'  => 'image',
+        ],
+        'og_description' => [
+            'caption' => 'OpenGraph description',
+            'type'  => 'textareamini',
+        ],
+    ],
+];
